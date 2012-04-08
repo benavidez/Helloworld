13c52a747cd43e7026e3e3baadf6df7a6fe52a9c
Eduardo Benavidez <eduardob@slac.stanford.edu>
2012-04-08 11:47:28 -0700

    initial commit with FooTester and Foo

---
+++ Foo.py
@@ -0,0 +1,8 @@
+'''
+Created on Apr 8, 2012
+
+@author: eduardob
+'''
+
+def foo(n):
+    pass
---
+++ FooTester.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 8, 2012
+
+@author: eduardob
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
