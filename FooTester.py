0fc01d29c148b85923987ee39ee9817eebfde8a5
Eduardo Benavidez <eduardob@slac.stanford.edu>
2012-04-08 12:04:37 -0700

    added 3 basic tests that fail

--- FooTester.py
+++ FooTester.py
@@ -4,15 +4,19 @@ Created on Apr 8, 2012
 @author: eduardob
 '''
 import unittest
+import Foo
+
+class FooTester(unittest.TestCase):
 
 
-class Test(unittest.TestCase):
-
-
-    def testName(self):
-        pass
-
-
+    def testFoo(self):
+        res = Foo.foo(18)
+        self.assertEqual(res, 'fizz', res)
+        res = Foo.foo(15)
+        self.assertEqual(res, 'buzz', res)
+        res = Foo.foo(555)        
+        self.assertEqual(res, 'fizzbuzz', res)
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
