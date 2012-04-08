9250dc301c44b3d83ae779013d0c276fe0707e29
Eduardo Benavidez <eduardob@slac.stanford.edu>
2012-04-08 12:25:13 -0700

    added more tests

--- FooTester.py
+++ FooTester.py
@@ -16,6 +16,13 @@ class FooTester(unittest.TestCase):
         self.assertEqual(res, 'buzz', res)
         res = Foo.foo(555)        
         self.assertEqual(res, 'fizzbuzz', res)
+        # test not divisible by 3 or 5
+        res = Foo.foo(8)        
+        self.assertEqual(res, '', res)
+        res = Foo.foo(1)
+        self.assertEqual(res, '', res)
+        res = Foo.foo(0)        
+        self.assertEqual(res, '', res)
     
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
